813204193c355ba1943c6f04753afeb47bba1380
Vaynah <154355759+script0x0kiddy@users.noreply.github.com>
2026-02-25 22:45:26 +0300

    Небольшие, но важные изменения
    Версия v0.1:
    
    Базовые команды:
    
    - Вывести список задач
    - Добавить/Изменить/Удалить задачу
    - Выйти из программы санкционно
    
    ==================================
    
    Версия v0.2:
    
    Малые изменения:
    
    1. Отловлены практически все ошибки
    2. Исправлен баг №2 (Задача без имени)
    3. Добавлена возможность удалять все задачи разом
    4. Добавлены комментарии+
    
    Проблемы и задачи:
    
    1. Отловить все ошибки в скрипте
    2. Баг №2 с задачей без имени
    3. Возможность удалить все задачи разом

--- task_manager_v0.1.py
+++ task_manager_v0.1.py
@@ -1,11 +1,11 @@
 from time import sleep
 
 print()
-print('==========================')
+print('=======Добро пожаловать========')
 print('1. Вывести список задач')
 print('2. Добавить')
 print('3. Изменить')
-print('4. Удалить задачу')
+print('4. Удалить')
 print('5. Закрыть <Task Manager>')
 print('==========================')
 
@@ -44,34 +44,35 @@ while select != 5:
 	elif select == 2:
 		print()
 		print(f'Выбрано: Добавить задачу')
-		sleep(1)
 		print()
-
+		
 		new_task_name = str(input('Введите название задачи: '))
 
-		# Добавление в список задач
-		tasks.append(new_task_name.capitalize())
-		sleep(1)
-		print('Задача успешно добавлена!')
-		sleep(1)
-		print('Чтобы проверить нажмите <1>')
-		sleep(1)
-		print()
+		# Обработка имени новой задачи
+		while new_task_name == '':
+			print("❌ Заполните пустое поле!")
+			new_task_name = str(input('Введите название задачи: '))
+		else:
+			# Добавление в список задач
+			tasks.append(new_task_name.capitalize())
+			print('Задача успешно добавлена!')
+			print('Чтобы проверить нажмите <1>')
+			sleep(1)
+			print()
 
 	# Изменить
 	elif select == 3:
 		print()
 		print(f"Выбрано: Изменить задачу")
-		sleep(1)
 		print()
 
-		chg_task_num = int(input("Введите номер изменяемой задачи: "))
+		chg_task_num = str(input("Введите номер изменяемой задачи: "))
 		select_task = ''
 
 		# Перебор списка задач с номером chg_task_num
 		for i in range(0, len(tasks)):
 			# Если номер введённой задачи найден в списке задач
-			if chg_task_num - 1 == i:
+			if int(chg_task_num) - 1 == i:
 				select_task = tasks[i]
 
 		if select_task == '':
@@ -81,52 +82,94 @@ while select != 5:
 			print()
 		else:
 			print(f"Выбрана: <{select_task}>")
-			sleep(1)
 			print('--------------------')
 			print("1. Отметить выполненой\n2. Изменить название")
 			print('--------------------')
 
-			chg_task = int(input("Выберите действие над задачей: "))
-			print()
+			chg_task = str(input("Выберите действие над задачей: "))
 
-			if chg_task == 1:
-				tasks[i] = select_task
-				print(f'Задача отмечена выполненой: {select_task}')
-				sleep(1)
-				print()
-			elif chg_task == 2:
+			if int(chg_task) == 1:
+				if tasks[i] != (select_task + " \u2705"):
+					print("⚠️ Задача уже выполнена!")
+					chg_task = str(input("Выберите действие над задачей: "))
+				else:
+					tasks[i] = select_task + " \u2705"
+					print(f'Задача <{select_task}> отмечена выполненой')
+					print()
+
+			elif int(chg_task) == 2:
 				select_task = str(input("Введите новое название задачи: "))
 				tasks[i] = select_task
-				print(f'Новое название: {tasks[i]}')
+				print(f'Название задачи изменено на: <{tasks[i]}>')
+				sleep(1)
+				print()
 
 	# Удалить задачу
 	elif select == 4:
-		print()
 		print(f'Выбрано: Удалить задачу')
-		sleep(1)
 		print()
+		print('--------------------')
+		print("1. Удалить конкретную задачу\n2. Удалить все задачи")
+		print('--------------------')
 
-		del_task_num = int(input("Введите номер удаляемой задачи: "))
-		del_task = ''
+		del_select = str(input("Выберите действие: "))
 
-		for i in range(0, len(tasks)):
-			if del_task_num - 1 == i:
-				del_task = tasks[i]
+		if del_select != '':
+			if int(del_select) == 1:
+				del_task_num = str(input("Введите номер удаляемой задачи: "))
+				del_task = ''
 
-		if del_task == '':
-			print()
-			print('Нет такой задачи.')
-			print()
+				# Перебор списка задач с номером del_task
+				for i in range(0, len(tasks)):
+					# Если номер удаляемой задачи и номер существующей задачи совпал
+					if int(del_task_num) - 1 == i:
+						# Вытаскиваем из цикла удаляемую задачу
+						del_task = tasks[i]
+
+				# Обработка удаляемой задачи
+				if del_task == '':
+					print()
+					print('Нет такой задачи.')
+					print()
+				else:
+					print(f'Будет удалена задача: {del_task}')
+					delete = str(input('Удалить? (Y/N): '))
+
+					if delete == 'Y' or delete == 'y':
+						tasks.remove(del_task)
+						print(f'Задача {del_task} удалена.')
+						sleep(1)
+						print()
+					elif delete == 'N' or delete == 'n':
+						print('Отмена удаления задачи.')
+
+			elif int(del_select) == 2:
+				if tasks != []:
+					print(f'Будут удалены следующие задачи: ')
+
+					for task in tasks:
+						print(f"--> {task}")
+					
+					delete = str(input('Удалить? (Y/N): '))
+
+					if delete == 'Y' or delete == 'y':
+						tasks = []
+						print(f'Список задач очищен.')
+						sleep(1)
+						print()
+					elif delete == 'N' or delete == 'n':
+						print('Отмена очистки списка.')
+					else:
+						print("⚠️  Введите символ Y/y или N/n")
+						print()
+				else:
+					print()
+					print('ℹ️  Пустой список нельзя очистить.')
+					sleep(1)
 		else:
-			print(f'Будет удалена задача: {del_task}')
-			delete = str(input('Удалить? (Y/N): '))
-
-			if delete == 'Y' or delete == 'y':
-				tasks.remove(del_task)
-				print(f'Задача {del_task} удалена.')
-				print()
-			elif delete == 'N' or delete == 'n':
-				print('Отмена удаления задачи.')
+			while del_select == '':
+				print("❌ Ошибка. Вы ввели не цифру.")
+				del_select = str(input("Выберите действие: "))
 
 	# Вывод меню Task Manager'a
 	print('==========================')
